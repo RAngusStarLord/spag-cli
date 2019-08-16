59d37ab1c2182603a1842e0145676ea41b515557
Ruaridh Angus <ruaridh.angus1@vodafone.com>
2019-08-16 10:43:58 +0100

    bumping versions

--- twine_upload.sh
+++ twine_upload.sh
@@ -5,4 +5,8 @@ bumpversion --current-version $version patch ./setup.py
 
 git commit -am "bumping versions"
 
+pip uninstall dist/* -y
+rm -fr dist/
+python setup.py bdist_wheel
+
 python -m twine upload dist/* --verbose
